494e8c9cb25096f18bead53f5b7d9019adfe805f
Abiel Isak <abizmo99@gmail.com>
2023-01-11 15:53:02 +0100

    Loops over name

--- index.js
+++ index.js
@@ -17,7 +17,33 @@ console.log(`The driver has the longest name, it has ${driver.length} characters
   console.log(`Wow, you both have equally long names, ${driver.length} characters!`)
 }
 
-
-
-
 // Iteration 3: Loops
+
+let upperName =""
+
+for (let i = 0; i < driver.length; i++) {
+    upperName += driver[i].toUpperCase() + " ";
+}
+
+console.log(upperName)
+
+let reverseName = ""
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+    reverseName += hacker2[i];
+}
+console.log(reverseName)
+
+
+for (let i = 0; i < driver.length && i < hacker2.length; i++) {
+    if (driver[i] < hacker2[i]) {
+        console.log("The driver's name goes first");
+        break;
+    } else if (driver[i] > hacker2[i]) {
+        console.log("Yo, the navigator goes first definitely");
+        break;
+    }else{
+      console.log("What?! You both have the same name?");
+      break;
+    }
+}
