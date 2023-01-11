cb1d32337c3630d9b7e1cf9f9a8eaa509a5b6c88
Abiel Isak <abizmo99@gmail.com>
2023-01-11 15:33:00 +0100

    Compare name length

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Iteration 1: Names and Input
 const driver="Charlie"
 console.log(`The driver's name is ${driver}`)
 
@@ -6,6 +7,17 @@ console.log(`The navigator's name is ${hacker2}`)
 
 
 // Iteration 2: Conditionals
+
+if (driver.length > hacker2.length){
+  
+console.log(`The driver has the longest name, it has ${driver.length} characters`)
+}else if(driver.length < hacker2.length){
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+} else{
+  console.log(`Wow, you both have equally long names, ${driver.length} characters!`)
+}
+
+
 
 
 // Iteration 3: Loops
